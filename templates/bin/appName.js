7cb6abdf86ee490be946d8765cee36e0841c9380
Alex Mingoia <talk@alexmingoia.com>
2014-09-07 18:52:01 -0700

    Use JSDoc ignore comment syntax.

--- templates/bin/appName.js
+++ templates/bin/appName.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-/*
+/*!
  * <%= appNameSlug %>
  * https://github.com/<%= userName %>/<%= appNameSlug %>
  *
--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,4 +1,4 @@
-/*
+/*!
  * <%= appNameSlug %>
  * https://github.com/<%= userName %>/<%= appNameSlug %>
  *
